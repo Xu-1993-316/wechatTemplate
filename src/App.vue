e0e11a03fe092af8d4e564d7d0e9d3b8f1230e86
许传金 <864157164@qq.com>
2019-08-12 17:29:45 +0800

    上传修改页面初始布局的代码

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <transition :name="animation">
-      <router-view class="page-container" comp-type="page"></router-view>
+      <router-view class="page-container"
+                   comp-type="page"></router-view>
     </transition>
     <!-- 两个固定埋点 -->
-    <input type="hidden" input-tag="main_source" value="wechat">
-    <input type="hidden" input-tag="project_name" value="test" />
+    <input type="hidden"
+           input-tag="main_source"
+           value="wechat">
+    <input type="hidden"
+           input-tag="project_name"
+           value="test" />
   </div>
 </template>
 <script>
 export default {
   name: "app",
-  data: function() {
+  data: function () {
     return {
       animation: "fade"
     };
@@ -23,7 +28,7 @@ export default {
   margin: 0px;
   padding: 0px;
   box-sizing: border-box;
-  font-family: Arial;
+  font-family: "Microsoft YaHei";
 }
 
 html,
@@ -36,12 +41,5 @@ body {
   position: relative;
   height: 100%;
   width: 100%;
-  /* overflow: scroll;
-  -webkit-overflow-scrolling: touch */
-}
-
-.page > main{
-  height: 100%;
-  overflow: scroll;
 }
 </style>
--- src/page/indexPage/index.vue
+++ src/page/indexPage/index.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="page">
-    <h2>{{message}}</h2>
-    <button v-safeClick="{fn: toSub, params: {name:message}}">第二页</button>
-    <button @click="toThird">第三页</button>
+    <div class="content_box">
+      <div class="content">
+        <h2>{{message}}</h2>
+        <button v-safeClick="{fn: toSub, params: {name:message}}">第二页</button>
+        <button @click="toThird">第三页</button>
+      </div>
+    </div>
+    <div class="bottom_box"></div>
   </div>
 </template>
 <script src="./script.js"></script>
